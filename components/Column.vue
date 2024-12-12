fa62638e93b28cb1a760bc189986b5b82a38b892
@Akirienko <lakirienkol@gmail.com>
2024-12-12 18:46:23 +0200

    added visible place for card

--- components/Column.vue
+++ components/Column.vue
@@ -11,29 +11,54 @@ const props = defineProps({
 
 const todoStore = useTodoStore();
 const taskModal = ref(false);
+const hoveredColumnId = ref(0);
+const storedId = ref()
+
+function handleDragOver(event, columnId) {
+  event.preventDefault();
+
+  if (columnId === hoveredColumnId.value) {
+    return;
+  }
+
+  hoveredColumnId.value = columnId;
+
+}
 
 function handleDrop(event) {
   const taskData = event.dataTransfer.getData('task');
   const fromColumnId = event.dataTransfer.getData('fromColumnId');
 
   let task = JSON.parse(taskData);
-  console.log('event.dataTransfer.getData', event.dataTransfer.getData);
-
-  console.log('col.task', task);
-  console.log('col.fromColumnId', fromColumnId);
-  console.log('col.props.column.id', props.column.id);
-
-
 
   todoStore.moveTask(task, fromColumnId, props.column.id);
+
+  hoveredColumnId.value = '';
 }
+
+function handleDragLeave(event, columnId) {
+  const rect = event.currentTarget.getBoundingClientRect();
+
+  const isOutside = (
+    event.clientX < rect.left ||
+    event.clientX > rect.right ||
+    event.clientY < rect.top ||
+    event.clientY > rect.bottom
+  );
+
+  if (isOutside) {
+    hoveredColumnId.value = '';
+  }
+}
+
 </script>
 
 <template>
   <div
     class="bg-[#e9f1ff] rounded-lg shadow-md p-4 w-80"
-    @dragover.prevent
+    @dragover="handleDragOver($event, column.id)"
     @drop="handleDrop"
+    @dragleave="handleDragLeave($event, column.id)"
   >
     <h2 class="font-bold mb-4">{{ column.name }}</h2>
     <div class="space-y-2">
@@ -42,7 +67,12 @@ function handleDrop(event) {
         :key="task"
         :task="task"
         :columnId="column.id"
+
       />
+      <div
+        v-if="hoveredColumnId === column.id"
+        class="h-12 bg-blue-100 border-2 border-dashed border-blue-400 rounded"
+      ></div>
     </div>
     <button
       class="mt-5 bg-blue-500 text-white px-2 py-1 rounded hover:bg-blue-600"
@@ -51,6 +81,7 @@ function handleDrop(event) {
       Add a task
     </button>
   </div>
+
 
   <AddTask v-if="taskModal" :columnId="column.id" @close-addtask-modal="taskModal=false" />
 </template>
